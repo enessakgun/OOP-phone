879bc9308b540a03ce3a321d00027d9314e128fb
enes akgüm <enes0303enes@gmail.com>
2023-08-04 17:59:44 +0300

    commit 5

--- OOP-phone/OOP-phone/Program.cs
+++ OOP-phone/OOP-phone/Program.cs
@@ -14,16 +14,12 @@ namespace OOP_phone
     internal class Program
     {
 
-       
-
         static void Main(string[] args)
-            {
-            
-            sqlconnection sqlconnection = new sqlconnection();
-            sqlconnection.sqlopen();
+        {
+
            
 
-            
+            anamenu:
             xioami xioami = new xioami();
             samsung samsung = new samsung();
             iphone iphone = new iphone();
@@ -31,177 +27,227 @@ namespace OOP_phone
             while (true)
             {
                 Console.Clear();
-                Console.WriteLine("Lütfen incelemek istediğiniz markayı giriniz. Çıkmak için 4'e basınız.");
-                Console.WriteLine("1-iphone 2-xioami 3-samsung");
+                Console.WriteLine("------ Telefon Uygulaması ------");
+                Console.WriteLine("1 - Yeni telefon ekle");
+                Console.WriteLine("2 - Varolan telefonları incele");
+                Console.WriteLine("0 - Çıkış yap");
+                Console.Write("Seçiminizi yapın: ");
 
-                string secim = Console.ReadLine();
-
-                if (secim == "1")
+                string secimana = Console.ReadLine();
+                switch (secimana)
                 {
-                    while (true)
-                    {
-                        basadon:
-                        Console.Clear();
-                        iphone.iphonemodelleri();
-                        string secim1 = Console.ReadLine();
+                    case "0":
+                        Environment.Exit(0);
+                        break;
 
-                        switch (secim1)
+                    case "1":
+                        Console.WriteLine("eklemek istedigin markayı seç 1-xioami-2-samsung-3-apple. ");
+                        string markasec = Console.ReadLine();
+                        switch (markasec)
                         {
                             case "1":
-                                iphone.iphonex();
                                 break;
                             case "2":
-                                iphone.iphone11();
                                 break;
                             case "3":
-                                iphone.iphone14();
                                 break;
-                            case "4":
+                            default:
+                                Console.WriteLine("Geçersiz seçim. Lütfen tekrar deneyin.");
+                                Console.ReadKey();
+                                goto anamenu;
+                        }
+
+
+                        sqlconnection sqlconnection = new sqlconnection();
+                        sqlconnection.sqlopen();
+                        break;
+
+                    case "2":
+
+                        while (true)
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Lütfen incelemek istediğiniz markayı giriniz. Çıkmak için 4'e basınız.");
+                            Console.WriteLine("1-iphone 2-xioami 3-samsung");
+
+                            string secim = Console.ReadLine();
+
+                            if (secim == "1")
+                            {
+                                while (true)
+                                {
+                                basadon:
+                                    Console.Clear();
+                                    iphone.iphonemodelleri();
+                                    string secim1 = Console.ReadLine();
+
+                                    switch (secim1)
+                                    {
+                                        case "1":
+                                            iphone.iphonex();
+                                            break;
+                                        case "2":
+                                            iphone.iphone11();
+                                            break;
+                                        case "3":
+                                            iphone.iphone14();
+                                            break;
+                                        case "4":
+                                            Environment.Exit(0);
+                                            break;
+                                        case "5":
+                                            break;
+
+                                        default:
+                                            Console.WriteLine("Hatalı giriş. Enter'a bas.");
+                                            Console.ReadKey();
+                                            goto basadon;
+                                    }
+                                    if (secim1 == "5")
+                                        break;
+                                    basadon1:
+                                    Console.WriteLine("cıkış için4 e bir üst menü için 5 e basınız.");
+                                    string secim2 = Console.ReadLine();
+                                    switch (secim2)
+                                    {
+                                        case "4":
+                                            Environment.Exit(0);
+                                            break;
+                                        case "5":
+                                            break;
+                                        default:
+                                            Console.WriteLine("Hatalı giriş. Enter'a bas.");
+                                            Console.ReadKey();
+                                            goto basadon1;
+                                    }
+
+                                }
+                            }
+                            else if (secim == "2")
+                            {
+                                while (true)
+                                {
+                                basadon:
+                                    Console.Clear();
+                                    xioami.xioamimodelleri();
+                                    string secim1 = Console.ReadLine();
+
+                                    switch (secim1)
+                                    {
+                                        case "1":
+                                            xioami.Redmi9();
+                                            break;
+                                        case "2":
+                                            xioami.Redmi10();
+                                            break;
+                                        case "3":
+                                            xioami.note10();
+                                            break;
+                                        case "4":
+                                            Environment.Exit(0);
+                                            break;
+                                        case "5":
+                                            break;
+                                        default:
+                                            Console.WriteLine("Hatalı giriş. Enter'a bas.");
+                                            Console.ReadKey();
+                                            goto basadon;
+
+                                    }
+                                    if (secim1 == "5")
+                                        break;
+                                    basadon1:
+                                    Console.WriteLine("cıkış için4 e bir üst menü için 5 e basınız.");
+                                    string secim2 = Console.ReadLine();
+                                    switch (secim2)
+                                    {
+                                        case "4":
+                                            Environment.Exit(0);
+                                            break;
+                                        case "5":
+                                            break;
+                                        default:
+                                            Console.WriteLine("Hatalı giriş. Enter'a bas.");
+                                            Console.ReadKey();
+                                            goto basadon1;
+                                    }
+
+                                }
+                            }
+                            else if (secim == "3")
+                            {
+                                while (true)
+                                {
+                                basadon:
+                                    Console.Clear();
+                                    samsung.samsungmodelleri();
+                                    string secim1 = Console.ReadLine();
+
+                                    switch (secim1)
+                                    {
+                                        case "1":
+                                            samsung.s22();
+                                            break;
+                                        case "2":
+                                            samsung.note10();
+                                            break;
+                                        case "3":
+                                            samsung.note11();
+                                            break;
+                                        case "4":
+                                            Environment.Exit(0);
+                                            break;
+                                        case "5":
+                                            break;
+                                        default:
+                                            Console.WriteLine("Hatalı giriş. Enter'a bas.");
+                                            Console.ReadKey();
+                                            goto basadon;
+                                    }
+                                    if (secim1 == "5")
+                                        break;
+                                    basadon1:
+                                    Console.WriteLine("cıkış için 4 e bir üst menü için 5 basınız.");
+                                    string secim2 = Console.ReadLine();
+                                    switch (secim2)
+                                    {
+                                        case "4":
+                                            Environment.Exit(0);
+                                            break;
+                                        case "5":
+                                            break;
+                                        default:
+                                            Console.WriteLine("Hatalı giriş. Enter'a bas.");
+                                            Console.ReadKey();
+                                            goto basadon1;
+
+                                    }
+
+                                }
+                            }
+                            else if (secim == "4")
+                            {
                                 Environment.Exit(0);
-                                break;
-                            case "5":
-                                break;
-                        
-                            default:
+                            }
+                            else
+                            {
                                 Console.WriteLine("Hatalı giriş. Enter'a bas.");
                                 Console.ReadKey();
-                                goto basadon;
+                            }
                         }
-                        if (secim1 == "5")
-                            break;
-                        basadon1:
-                        Console.WriteLine("cıkış için4 e bir üst menü için 5 e basınız.");
-                        string secim2 = Console.ReadLine();
-                        switch(secim2)
-                        {
-                            case "4":
-                             Environment.Exit(0);
-                                break;
-                            case "5":
-                            break;
-                            default:
-                                Console.WriteLine("Hatalı giriş. Enter'a bas.");
-                                Console.ReadKey();
-                                goto basadon1;
-                        }
+                    default:
+                        Console.WriteLine("Geçersiz seçim. Lütfen tekrar deneyin.");
+                        Console.ReadKey();
+                        goto anamenu;
 
-                    }
                 }
-                else if (secim == "2")
-                {
-                    while (true)
-                    { 
-                        basadon:
-                        Console.Clear();
-                        xioami.xioamimodelleri();
-                        string secim1 = Console.ReadLine();
+                break;
+                
+            }
+           
 
-                        switch (secim1)
-                        {
-                            case "1":
-                                xioami.Redmi9();
-                                break;
-                            case "2":
-                                xioami.Redmi10();
-                                break;
-                            case "3":
-                                xioami.note10();
-                                break;
-                            case "4":
-                                Environment.Exit (0);
-                                break;
-                            case "5":
-                                break;
-                            default:
-                                Console.WriteLine("Hatalı giriş. Enter'a bas.");
-                                Console.ReadKey();
-                                goto basadon;
-                                
-                        }
-                        if (secim1 == "5")
-                            break;
-                        basadon1:
-                        Console.WriteLine("cıkış için4 e bir üst menü için 5 e basınız.");
-                        string secim2 = Console.ReadLine();
-                        switch (secim2)
-                        {
-                            case "4":
-                                Environment.Exit(0);
-                                break;
-                            case "5":
-                                break;
-                            default:
-                                Console.WriteLine("Hatalı giriş. Enter'a bas.");
-                                Console.ReadKey();
-                               goto basadon1;
-                        }
-
-                    }
-                }
-                else if (secim == "3")
-                {
-                    while (true)
-                    {
-                        basadon:
-                        Console.Clear();
-                        samsung.samsungmodelleri();
-                        string secim1 = Console.ReadLine();
-
-                        switch (secim1)
-                        {
-                            case "1":
-                                samsung.s22();
-                                break;
-                            case "2":
-                                samsung.note10();
-                                break;
-                            case "3":
-                                samsung.note11();
-                                break;
-                            case "4":
-                                Environment.Exit(0);
-                                break;
-                            case "5":
-                                break;
-                            default:
-                                Console.WriteLine("Hatalı giriş. Enter'a bas.");
-                                Console.ReadKey();
-                                goto basadon;
-                        }
-                        if (secim1 == "5")
-                            break;
-                        basadon1:
-                        Console.WriteLine("cıkış için 4 e bir üst menü için 5 basınız.");
-                        string secim2 = Console.ReadLine();
-                        switch (secim2)
-                        {
-                            case "4":
-                                Environment.Exit(0);
-                                break;
-                            case "5":
-                                break;
-                            default:
-                                Console.WriteLine("Hatalı giriş. Enter'a bas.");
-                                Console.ReadKey();
-                                goto basadon1;
-
-                        }
-
-                    }
-                }
-                else if (secim == "4")
-                {
-                    Environment.Exit(0);
-                }
-                else
-                {
-                    Console.WriteLine("Hatalı giriş. Enter'a bas.");
-                    Console.ReadKey();
-                }
-            }  
-            
         }
-    }        
+    }
 }
 
 
